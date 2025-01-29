765142194dc9e7baf1f32fda883761836627b727
jairomedac <flakkbashoo@gmail.com>
2025-01-29 10:56:05 +0100

    nuevos arreglos php

--- 07_apis/estudios/index.php
+++ 07_apis/estudios/index.php
@@ -10,17 +10,46 @@
     ?>
 </head>
 <body>
+    <form action="" methof="get">
+        <label>Ciudad: </label>
+        <input type="text" name="ciudad">
+        <input type="submit" value="Buscar">
+    </form>
+
     <?php
     $apiUrl = "http://localhost/Ejercicios/07_apis/estudios/api_estudios.php";
+    if(isset($_GET["ciudad"])){
+        $ciudad = $_GET["ciudad"];
+        $apiUrl = "$apiUrl?ciudad=$ciudad";
+    }
     $curl = curl_init();
-    // Iniciamos el curl con una URL, que va a ser $apiUrl
     curl_setopt($curl, CURLOPT_URL, $apiUrl);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $respuesta = curl_exec($curl);
     curl_close($curl);
 
+    
     $estudios = json_decode($respuesta, true);
-    print_r($estudios);
+ 
     ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Estudio</th>
+                <th>Ciudad</th>
+                <th>Año de fundación</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach($estudios as $estudio) { ?>
+                <tr>
+                    <td><?php echo $estudio["nombre_estudio"] ?></td>
+                    <td><?php echo $estudio["ciudad"] ?></td>
+                    <td><?php echo $estudio["anno_fundacion"] ?></td>
+                </tr>
+            <?php }?>
+        </tbody>
+    </table>
 </body>
 </html>
---
+++ 07_apis/jkan/top_anime.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top Anime</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
+    ?>
+</head>
+<body>
+    
+    <?php
+       $apiUrl = "https://api.jikan.moe/v4/top/anime";
+       if(isset($_GET["type"])){
+           $tipo = $_GET["tipo"];
+           $apiUrl = "https://api.jikan.moe/v4/top/anime?type=$tipo";
+       }
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+        $datos = json_decode($respuesta, true);
+        $animes = $datos["data"];
+    ?>
+  <form action="top_anime.php" method="GET">
+                    <div class="input-name">
+                        <label>Filtrar por:</label><br><br>
+                        <input type="radio" name="radiogroup1" id="radioo" class="radio-button">
+                        <label for="ravi" class="serie">Serie</label>
+                        <input type="radio" name="radiogroup" id="radioo" class="radio-button">
+                        <label for="ravi1" class="pelicula">Pelicula</label>
+                        <input type="radio" name="radiogroup" id="radioo" class="radio-button">
+                        <label for="radio" class="Todos">Todos</label>
+                    </div>
+                <br>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Posición</th>
+                <th scope="col">Título</th>
+                <th scope="col">Nota</th>
+                <th scope="col">Imagen</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach($animes as $anime) { ?>
+                    <tr>
+                        <td scope="row"><?php echo $anime["rank"]?></td>
+                        <td>
+                            <a href="anime.php?id=<?php echo $anime["mal_id"]?>">
+                                <?php echo $anime["title"]?>
+                            </a>
+                        </td scope="row">
+                        <td scope="row"><?php echo $anime["score"]?></td>
+                        <td scope="row">
+                            <img width="100px" src="<?php echo $anime["images"]["jpg"]["image_url"]?>">
+                        </td>
+                    </tr>
+                <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
